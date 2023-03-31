11e8a0b6afed8263ddf2454ce3e579ad71d5f426
LAPTOP-LJJM05JL\DDB-916644-Widdesto <widdesto.yudistiro@gmail.com>
2023-03-31 13:41:41 +0700

    feat: tambah path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components')
+    }
+  },
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'rvcoba',
+      fileName: (format) => `rvcoba.${format}.js`,
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
 })
